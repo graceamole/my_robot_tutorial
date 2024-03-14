127e3d8a176c1812ae372433763ead86fae0fc8a
Grace Amole <gracey1235@gmail.com>
2024-03-14 15:59:18 +0000

    Create action_client.py

---
+++ action_client.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+#import math
+import rospy
+import actionlib
+
+from my_robot_tutorial.msg import Navigate2DAction, Navigate2DFeedback, Navigate2DResult
+from geometry_msgs.msg import Point
